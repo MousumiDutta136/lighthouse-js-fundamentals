08109187cb039041185e8f95451432b0c1d5f484
MousumiDutta136 <dutta.mousumi136@gmail.com>
2021-10-28 14:09:06 -0400

    Function code

---
+++ hello.js
@@ -0,0 +1,14 @@
+// First program for Chapter function
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+//second program 
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Mousumi");
+sayHello("Rishan");
