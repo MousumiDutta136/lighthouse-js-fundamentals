31d61ba468a8e7624c967cba75efa734d2c6fbdd
MousumiDutta136 <dutta.mousumi136@gmail.com>
2021-10-28 16:11:52 -0400

    isOdd Function code

---
+++ odd.js
@@ -0,0 +1,6 @@
+// Program for a number is odd or else.
+const isOdd = function(num){
+  return num % 2 !== 0;
+}
+console.log(isOdd(3)); // return true as 3 is odd number
+console.log(isOdd(8)); // return false as 8 is even not odd number
