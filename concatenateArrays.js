68db248412dd6695357530a5d3bcf75adda79ffb
MousumiDutta136 <dutta.mousumi136@gmail.com>
2021-10-30 17:41:22 -0400

    Concatenation of two arrays into a singlearray

---
+++ concatenateArrays.js
@@ -0,0 +1,10 @@
+//concatenate the multiple arrays into a single array
+function concat(arr1, arr2){
+  let finalArray = arr1.concat(arr2);
+  return finalArray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]));
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]));
+console.log(concat([], [ 9, 7, 2 ]));
+console.log(concat([ 5, 10 ], []));
